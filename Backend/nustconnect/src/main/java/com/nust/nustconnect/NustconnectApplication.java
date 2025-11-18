201c2181fc3d43c3d0079b1a3a017cc13fc2e466
tmalik <tmalik.bese24seecs@seecs.edu.pk>
2025-11-18 14:07:23 +0500

    initial backend structure creation

---
+++ Backend/nustconnect/src/main/java/com/nust/nustconnect/NustconnectApplication.java
@@ -0,0 +1,13 @@
+package com.nust.nustconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NustconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NustconnectApplication.class, args);
+	}
+
+}
---
+++ Backend/nustconnect/src/test/java/com/nust/nustconnect/NustconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nust.nustconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NustconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
